5e4b4b93383b7e863bbf2902175f6873bf6e329d
NickoM31 <nicolasmathieu31130@gmail.com>
2017-11-25 11:37:31 +0100

    1er commit

---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace OC\PlatformBundle\Controller;
+
+use OC\PlatformBundle\Entity\Advert;
+use OC\PlatformBundle\Entity\Image;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Validator\Constraints\DateTime;
+
+
+
+class AdvertController extends Controller
+{
+    public function indexAction($page)
+    {
+        $listAdverts = array(
+            array(
+                'title'   => 'Recherche développpeur Symfony',
+                'id'      => 1,
+                'author'  => 'Alexandre',
+                'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Mission de webmaster',
+                'id'      => 2,
+                'author'  => 'Hugo',
+                'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Offre de stage webdesigner',
+                'id'      => 3,
+                'author'  => 'Mathieu',
+                'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+                'date'    => new \Datetime())
+        );
+
+        return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
+            'listAdverts' => $listAdverts));
+
+    }
+
+    public function viewAction($id)
+    {
+       /* $advert = array(
+            'title'   => 'Recherche développpeur Symfony2',
+            'id'      => $id,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()
+        );
+
+        return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+            'advert' => $advert
+        ));*/
+
+       $repository = $this->getDoctrine()
+           ->getManager()
+           ->getRepository('OCPlatformBundle:Advert')
+           ;
+       $advert = $repository->find($id);
+
+       if (null === $advert){
+           throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+       }else{
+           return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+               'advert' =>$advert
+           ));
+       }
+    }
+
+    public function addAction(Request $request)
+    {
+
+        $advert = new Advert();
+        $advert->setTitle("J'ai la dalle !");
+        $advert->setAuthor('Kiyou');
+        $advert->setContent("fromage frais! c'est bon !");
+        $advert->setDate(new \DateTime());
+
+        $image = new Image();
+        $image->setUrl('http://sdz-upload.s3.amazonaws.com/prod/upload/job-de-reve.jpg');
+        $image->setAlt('Job de rêve');
+
+        $advert->setImage($image);
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($advert);
+        $em->flush();
+
+        if ($request->isMethod('Post')){
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+            return $this->redirectToRoute('oc_platform_view', array('id' => $advert->getId()));
+        }
+
+        return $this->render('OCPlatformBundle:Advert:add.html.twig', ['advert' => $advert]);
+    }
+
+    public function editAction($id, Request $request)
+    {
+        $advert = array(
+            'title'   => 'Recherche développpeur Symfony',
+            'id'      => $id,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()
+        );
+
+        return $this->render('OCPlatformBundle:Advert:edit.html.twig', array(
+            'advert' => $advert
+        ));
+    }
+
+    public function deleteAction($id)
+    {
+        return $this->render('OCPlatformBundle:Advert:delete.html.twig');
+    }
+
+   /* public function viewSlugAction($slug, $year, $format)
+    {
+        return new Response("On pourrait afficher l'annonce correspondant au
+            slug '".$slug."', créée en ".$year." et au format ".$format."."
+        );
+    }*/
+
+    public function menuAction($limit)
+    {
+        $listAdverts = array(
+            array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+            array('id' => 5, 'title' => 'Mission de webmaster'),
+            array('id' => 9, 'title' => 'Offre de stage webdesigner')
+        );
+
+        return $this->render('OCPlatformBundle:Advert:menu.html.twig', array(
+            'listAdverts' => $listAdverts
+        ));
+   }
+
+    public function editImageAction($advertId)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        // On récupère l'annonce
+        $advert = $em->getRepository('OCPlatformBundle:Advert')->find($advertId);
+
+        // On modifie l'URL de l'image par exemple
+        $advert->getImage()->setUrl('test.png');
+
+        // On n'a pas besoin de persister l'annonce ni l'image.
+        // Rappelez-vous, ces entités sont automatiquement persistées car
+        // on les a récupérées depuis Doctrine lui-même
+
+        // On déclenche la modification
+        $em->flush();
+
+        return new Response('OK');
+    }
+}
